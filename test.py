3f99cdc720d12f8c24670de78a52133308831ec1
cflin <cflin@mail.cjcu.edu.tw>
2021-01-28 13:47:23 +0800

    使用vedio

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt 
 
-img = cv2.imread('test.jpg',0)
+img = cv2.imread('test.jpg',1)
+
+img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 print(img)
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic') 
+plt.imshow(img1) 
 plt.xticks([]), plt.yticks([]) 
 plt.show()
 
---
+++ test2.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+#Import only if not previously imported
+
+#Import only if not previously imported
+ 
+# Create a Video Reader Object.
+cap = cv2.VideoCapture(0)
+if cap.isOpened() == False:
+    print("Error in opening video stream or file")
+#Define the codec for the Video
+#fourcc = cv2.VideoWriter_fourcc("Fourcc Codec Eg-XVID")
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#Create Video Writer Object
+writer = cv2.VideoWriter('test.avi',fourcc, 30, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+         writer.write(frame)
+         cv2.imshow("Frame",frame)
+         # Exit on pressing esc
+         if cv2.waitKey(20) & 0xFF == 27:
+             break
+    else:
+         break
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
+
