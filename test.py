794df66b35b194e5b607aa1087ef9430e105b55a
cflin <cflin@mail.cjcu.edu.tw>
2021-01-28 16:04:22 +0800

    ROI練習

--- test.py
+++ test.py
@@ -4,9 +4,12 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('test.jpg',1)
 
-img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+roi=img[77:122,640:788]
+img[149:194,460:608]=roi
+# roi=img[200:400, 200:400]
+# img[100:300,100:300]=roi
 
-print(img)
+img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 plt.imshow(img1) 
 plt.xticks([]), plt.yticks([]) 
 plt.show()
