9be3999f9159b00296ce883a62c8fe620e14a17c
cflin <cflin@mail.cjcu.edu.tw>
2021-01-28 11:48:09 +0800

    用 plt

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt 
+
 img = cv2.imread('test.jpg',0)
 print(img)
-cv2.namedWindow('test',cv2.WINDOW_NORMAL)
-cv2.imshow('test',img)
-cv2.imwrite('test1.jpg',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic') 
+plt.xticks([]), plt.yticks([]) 
+plt.show()
+
+# cv2.namedWindow('test',cv2.WINDOW_NORMAL)
+# cv2.imshow('test',img)
+# cv2.imwrite('test1.jpg',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
