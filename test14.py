18a33cde0e6cdd29c8b92b28a4714a033b20ba00
cflin <cflin@mail.cjcu.edu.tw>
2021-01-30 16:13:21 +0800

    標記68點

--- test14.py
+++ test14.py
@@ -12,7 +12,7 @@ img = cv2.imread('akb.jpg')
 detector = dlib.get_frontal_face_detector()
 
 # 偵測人臉
-face_rects = detector(img, 0)
+face_rects, scores, idx = detector.run(img, 0, -1)
 
 # 取出所有偵測的結果
 for i, d in enumerate(face_rects):
@@ -20,9 +20,12 @@ for i, d in enumerate(face_rects):
   y1 = d.top()
   x2 = d.right()
   y2 = d.bottom()
+  text = "%2.2f(%d)" % (scores[i], idx[i])
 
   # 以方框標示偵測的人臉
   cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 4, cv2.LINE_AA)
+  cv2.putText(img, text, (x1, y1), cv2.FONT_HERSHEY_DUPLEX,
+            0.7, (255, 255, 255), 1, cv2.LINE_AA)
 
 # 顯示結果
 cv2.imshow("Face Detection", img)
